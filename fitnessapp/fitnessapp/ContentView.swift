ca4e89ed5daf25cf53011cdcd3358b3cd124fc3f
Lucas Aguiar <lucasabr2001@gmail.com>
2021-01-04 16:25:28 -0500

    HeY </3

--- fitnessapp/fitnessapp/ContentView.swift
+++ fitnessapp/fitnessapp/ContentView.swift
@@ -10,7 +10,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            Text(/*@START_MENU_TOKEN@*/"Workouts"/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Cardio"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+            Button(action: {}) {
+                Text(/*@START_MENU_TOKEN@*/"Leg"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Chest"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Shoulders"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Abs"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Biceps"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Triceps"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Back"/*@END_MENU_TOKEN@*/)
+                    .font(.title)
+                    .padding(.vertical, 30.0)
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
